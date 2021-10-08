7acfef35ccbbed9152adc3bfacf8a9a6a8e45205
ZDmitrii11 <dima_zol@mail.com>
2021-10-08 15:02:43 +0300

    piano

---
+++ pianino/src/App.vue
@@ -0,0 +1,29 @@
+<template>
+    <div id="app">
+
+
+        <pianino></pianino>
+
+
+    </div>
+</template>
+
+<script>
+
+    import pianino from "./components/pianino";
+
+    export default {
+        name: 'App',
+
+        components: {
+            pianino
+        }
+    }
+</script>
+
+<style scoped>
+    #app {
+
+    }
+
+</style>
---
+++ pianino/src/components/pianino.vue
@@ -0,0 +1,274 @@
+<template>
+    <div class="container" >
+        <svg
+
+                xmlns:dc="http://purl.org/dc/elements/1.1/"
+                xmlns:cc="http://creativecommons.org/ns#"
+                xmlns:rdf="http://www.w3.org/1999/02/22-rdf-syntax-ns#"
+                xmlns:svg="http://www.w3.org/2000/svg"
+                xmlns="http://www.w3.org/2000/svg"
+                xmlns:sodipodi="http://sodipodi.sourceforge.net/DTD/sodipodi-0.dtd"
+                xmlns:inkscape="http://www.inkscape.org/namespaces/inkscape"
+                width="1000px"
+                height="700px"
+                viewBox="0 0 472.36481 307.91974"
+                version="1.1"
+                id="svg4546"
+                inkscape:version="0.92.3 (2405546, 2018-03-11)"
+                sodipodi:docname="Piano Keys.svg"
+                inkscape:export-filename="C:\Users\matth\Downloads\Piano Keys.png"
+                inkscape:export-xdpi="96"
+                inkscape:export-ydpi="96">
+            <defs
+                    id="defs4540"/>
+            <sodipodi:namedview
+                    id="base"
+                    pagecolor="#ffffff"
+                    bordercolor="#666666"
+                    borderopacity="1.0"
+                    inkscape:pageopacity="0.0"
+                    inkscape:pageshadow="2"
+                    inkscape:zoom="0.49497475"
+                    inkscape:cx="381.09799"
+                    inkscape:cy="544.10454"
+                    inkscape:document-units="mm"
+                    inkscape:current-layer="g5221"
+                    showgrid="false"
+                    fit-margin-top="0"
+                    fit-margin-left="0"
+                    fit-margin-right="0"
+                    fit-margin-bottom="0"
+                    inkscape:window-width="1920"
+                    inkscape:window-height="1001"
+                    inkscape:window-x="-9"
+                    inkscape:window-y="-9"
+                    inkscape:window-maximized="1"/>
+            <metadata
+                    id="metadata4543">
+                <rdf:RDF>
+                    <cc:Work
+                            rdf:about="">
+                        <dc:format>image/svg+xml</dc:format>
+                        <dc:type
+                                rdf:resource="http://purl.org/dc/dcmitype/StillImage"/>
+                        <dc:title/>
+                    </cc:Work>
+                </rdf:RDF>
+            </metadata>
+            <g
+                    inkscape:label="Layer 1"
+                    inkscape:groupmode="layer"
+                    id="layer1"
+                    transform="translate(-27.060319,8.1503491)">
+                <g
+                        id="g5221">
+                    <g
+                            id="g905">
+                        <rect
+                                @click="playSound(notes[0].soundPath)"
+                                id="rect3724"
+                                width="67.084877"
+                                height="305.14905"
+                                x="28.445669"
+                                y="-6.7649989"
+                                style="fill:#ffffff;fill-opacity:1;stroke:#000000;stroke-width:2.77070045;stroke-linejoin:round;stroke-miterlimit:4;stroke-dasharray:none;stroke-opacity:0.98009949"
+                                ry="7.7258973"/>
+                        <rect
+                                @click="playSound(notes[1].soundPath)"
+                                id="rect3724-2"
+                                width="67.084877"
+                                height="305.14905"
+                                x="95.530548"
+                                y="-6.7649989"
+                                style="fill:#ffffff;fill-opacity:1;stroke:#000000;stroke-width:2.77070045;stroke-linejoin:round;stroke-miterlimit:4;stroke-dasharray:none;stroke-opacity:0.98009949"
+                                ry="7.7258978"/>
+                        <rect
+                                @click="playSound(notes[2].soundPath)"
+                                id="rect3724-9"
+                                width="67.084877"
+                                height="305.14905"
+                                x="162.61542"
+                                y="-6.7649989"
+                                style="fill:#ffffff;fill-opacity:1;stroke:#000000;stroke-width:2.77070045;stroke-linejoin:round;stroke-miterlimit:4;stroke-dasharray:none;stroke-opacity:0.98009949"
+                                ry="7.7258978"/>
+                        <rect
+                                @click="playSound(notes[3].soundPath)"
+                                id="rect3724-4"
+                                width="67.084877"
+                                height="305.14905"
+                                x="229.70029"
+                                y="-6.7649989"
+                                style="fill:#ffffff;fill-opacity:1;stroke:#000000;stroke-width:2.77070045;stroke-linejoin:round;stroke-miterlimit:4;stroke-dasharray:none;stroke-opacity:0.98009949"
+                                ry="7.7258978"/>
+                        <rect
+                                @click="playSound(notes[4].soundPath)"
+                                id="rect3724-8"
+                                width="67.084877"
+                                height="305.14905"
+                                x="296.78516"
+                                y="-6.7649989"
+                                style="fill:#ffffff;fill-opacity:1;stroke:#000000;stroke-width:2.77070045;stroke-linejoin:round;stroke-miterlimit:4;stroke-dasharray:none;stroke-opacity:0.98009949"
+                                ry="7.7258978"/>
+                        <rect
+                                @click="playSound(notes[5].soundPath)"
+                                id="rect3724-6"
+                                width="67.084877"
+                                height="305.14905"
+                                x="363.87003"
+                                y="-6.7649989"
+                                style="fill:#ffffff;fill-opacity:1;stroke:#000000;stroke-width:2.77070045;stroke-linejoin:round;stroke-miterlimit:4;stroke-dasharray:none;stroke-opacity:0.98009949"
+                                ry="7.7258978"/>
+                        <rect
+                                @click="playSound(notes[6].soundPath)"
+                                id="rect3724-28"
+                                width="67.084877"
+                                height="305.14905"
+                                x="430.9549"
+                                y="-6.7649989"
+                                style="fill:#ffffff;fill-opacity:1;stroke:#000000;stroke-width:2.77070045;stroke-linejoin:round;stroke-miterlimit:4;stroke-dasharray:none;stroke-opacity:0.98009949"
+                                ry="7.7258978"/>
+                        <rect
+                                @click="playSound(notes[7].soundPath)"
+                                style="fill:#000000;fill-opacity:1;stroke:#000000;stroke-width:2.82222223;stroke-linejoin:round;stroke-miterlimit:4;stroke-dasharray:none;stroke-opacity:1"
+                                id="rect5144"
+                                width="31.217339"
+                                height="182.76332"
+                                x="80.002808"
+                                y="-6.7392378"
+                                ry="7.7258859"/>
+                        <rect
+                                style="fill:#000000;fill-opacity:1;stroke:#000000;stroke-width:2.82222223;stroke-linejoin:round;stroke-miterlimit:4;stroke-dasharray:none;stroke-opacity:1"
+                                id="rect5144-5"
+                                width="31.217339"
+                                height="182.76332"
+                                x="147.05116"
+                                y="-6.7392378"
+                                ry="7.7258859"/>
+                        <rect
+                                style="fill:#000000;fill-opacity:1;stroke:#000000;stroke-width:2.82222223;stroke-linejoin:round;stroke-miterlimit:4;stroke-dasharray:none;stroke-opacity:1"
+                                id="rect5144-4"
+                                width="31.217339"
+                                height="182.76332"
+                                x="281.20731"
+                                y="-6.7392378"
+                                ry="7.7258859"/>
+                        <rect
+                                style="fill:#000000;fill-opacity:1;stroke:#000000;stroke-width:2.82222223;stroke-linejoin:round;stroke-miterlimit:4;stroke-dasharray:none;stroke-opacity:1"
+                                id="rect5144-8"
+                                width="31.217339"
+                                height="182.76332"
+                                x="348.2312"
+                                y="-6.7392378"
+                                ry="7.7258859"/>
+                        <rect
+                                style="fill:#000000;fill-opacity:1;stroke:#000000;stroke-width:2.82222223;stroke-linejoin:round;stroke-miterlimit:4;stroke-dasharray:none;stroke-opacity:1"
+                                id="rect5144-6"
+                                width="31.217339"
+                                height="182.76332"
+                                x="415.35974"
+                                y="-6.7392378"
+                                ry="7.7258859"/>
+                    </g>
+                </g>
+            </g>
+        </svg>
+
+        <div class="piano" v-for="note in notes" :key="note">
+          <div class="button-piano" @click.prevent="playSound(note.soundPath)">
+                <span class="span-notes">
+                {{note.name}}
+            </span>
+          </div>
+        </div>
+    </div>
+</template>
+
+<script>
+const note1 = require('../notes/noty-do.mp3');
+const note2 = require('../notes/re.mp3');
+const note3 = require('../notes/mi.mp3');
+const note4 = require('../notes/fa.mp3');
+const note5 = require('../notes/sol.mp3');
+const note6 = require('../notes/lja.mp3');
+const note7 = require('../notes/si.mp3');
+const noteAll = require('../notes/do-re-mi-fa-sol-lja-si.mp3')
+
+
+    export default {
+        name: "pianino",
+        data() {
+            return {
+                notes: [
+                    {
+                        name: 'Do',
+                        soundPath: note1
+                    },
+                    {
+                        name: 'Re',
+                        soundPath: note2
+                    },
+                    {
+                        name: 'Mi',
+                        soundPath: note3
+                    },
+                    {
+                        name: 'Fa',
+                        soundPath: note4
+                    },
+                    {
+                        name: 'Soli',
+                        soundPath: note5
+                    },
+                    {
+                        name: 'La',
+                        soundPath: note6
+                    },
+                    {
+                        name: 'Si',
+                        soundPath: note7
+                    },
+                    {
+                        name: 'AllPlay',
+                        soundPath: noteAll
+                    },
+
+                ]
+            }
+        },
+        methods:{
+            playSound (sound) {
+                let audio = new Audio(sound); // path to file
+                audio.play();
+            }
+        }
+
+    }
+</script>
+
+<style scoped>
+.container{
+    height: 250px;
+    width: 1200px;
+    display: flex;
+    flex-direction: row;
+
+}
+    .piano{
+        display: flex;
+        width: 150px;
+        height: 250px;
+        flex-direction: row;
+
+    }
+.button-piano{
+    width: 120px;
+    background-color: burlywood;
+    border: 1px solid black;
+    text-align: center;
+    justify-content: flex-end;
+}
+.button-piano>span {
+
+
+}
+</style>
