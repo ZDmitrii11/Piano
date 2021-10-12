6a855193e4af032c73d85c0e3332a51c40f16a18
ZDmitrii11 <dima_zol@mail.com>
2021-10-12 16:21:21 +0300

    modific

--- pianino/src/components/pianino.vue
+++ pianino/src/components/pianino.vue
@@ -1,24 +1,27 @@
 <template>
-    <div class="container" >
+    <div class="container">
 
         <div class="piano" v-for="note in notes" :key="note">
-          <div class="button-piano" @keypress="keySound(note.keyCode)"  :id="note.id" @click.prevent="playSound(note.soundPath)">
+            <div class="button-piano waves-effect " @keypress="keySound(note.keyCode)" :id="note.id"
+                 @click.prevent="playSound(note.soundPath)">
                 <span class="span-notes">
                 {{note.name}}
             </span>
-              <p>{{note.keyCode.toUpperCase()}}</p>
+                <p>{{note.keyCode.toUpperCase()}}</p>
 
-           <button class="str-button" @click.stop="trigger(note)">Click</button>
-          </div>
+                <button class="btn-small waves-effect waves-light" @click.stop="trigger(note)">
+                    Click
+                </button>
+            </div>
         </div>
         <div class="text-inp" v-if="isActive">
-             <h3>Change press key for your note {{currentTarget.name}} </h3>
-            <input type="text" style='text-transform:uppercase'  v-model="currentTarget.keyCode" maxlength="1">
-            <span class="error" v-if="error.length">{{error}}</span>
-            <button @click="isActive = false">cancel</button>
-            <button @click="changeKey()">ok</button>
-
-
+            <h5 :ref="currentTarget.name">Change press key for your note <span style="color: #ff0000">{{currentTarget.name}}</span></h5>
+            <input type="text" style='text-transform:uppercase' v-model="textKey" maxlength="1">
+             <div class="error" v-if="error.length">{{error}}</div>
+            <div class="buttons-input">
+                <button class="btn" @click="cancelButton">cancel</button>
+                <button class="btn" @click="changeKey">OK</button>
+            </div>
         </div>
 
 
@@ -26,165 +29,206 @@
 </template>
 
 <script>
-
+    // import M from 'materialize-css/'
     import name from '../notes'
-
-
 
     export default {
         name: "pianino",
         data() {
             return {
-                test:null,
-                isActive:false,
-                currentTarget:null,
-                textKey:'',
+                isActive: false,
+                currentTarget: null,
+                textKey: '',
+                error: '',
                 notes: [
                     {
-                        id:1,
+                        id: 1,
                         name: 'Do',
                         soundPath: name.Do,
-                        keyCode:''
+                        keyCode: ''
                     },
                     {
-                        id:2,
+                        id: 2,
                         name: 'Re',
                         soundPath: name.Re,
-                        keyCode:'',
+                        keyCode: '',
                     },
                     {
-                        id:3,
+                        id: 3,
                         name: 'Mi',
                         soundPath: name.Mi,
-                        keyCode:'',
+                        keyCode: '',
                     },
                     {
-                        id:4,
+                        id: 4,
                         name: 'Fa',
                         soundPath: name.Fa,
-                        keyCode:''
+                        keyCode: ''
                     },
                     {
-                        id:5,
+                        id: 5,
                         name: 'Soli',
                         soundPath: name.Sol,
-                        keyCode:''
+                        keyCode: ''
                     },
                     {
-                        id:6,
+                        id: 6,
                         name: 'La',
                         soundPath: name.La,
-                        keyCode:'',
+                        keyCode: '',
                     },
                     {
-                        id:7,
+                        id: 7,
                         name: 'Si',
                         soundPath: name.Si,
-                        keyCode:'',
+                        keyCode: '',
                     },
                     {
-                        id:8,
+                        id: 8,
                         name: 'AllPlay',
                         soundPath: name.allNotes,
-                        keyCode:''
+                        keyCode: ''
                     },
 
                 ],
-                error:'',
-                hhh:''
+
             }
         },
         created() {
-            console.log('name',name)
-            window.addEventListener('keypress',this.keySound)
+            window.addEventListener('keypress', this.keySound)
         },
-        methods:{
-            playSound (sound) {
+        methods: {
+            playSound(sound) {
                 let audio = new Audio(sound);
                 audio.play();
-
-
             },
-
-            keySound(e){
-               let sorted = this.notes.find(el=>el.keyCode === e.key)
-                   this.playSound(sorted.soundPath)
-                   let keyNote = document.getElementById(sorted.id)
-                    console.log(keyNote)
+            keySound(e) {
+                let sorted = this.notes.find(el => el.keyCode === e.key)
+                this.playSound(sorted.soundPath)
+                let keyNote = document.getElementById(sorted.id)
                 keyNote.classList.add('wave2')
-                setTimeout(()=>{
+                setTimeout(() => {
                     keyNote.classList.remove('wave2')
-                },1000)
-                },
-
-            trigger(ind){
-              this.isActive = !this.isActive
+                }, 1000)
+            },
+            trigger(ind) {
+                this.isActive = !this.isActive
                 this.currentTarget = ind
             },
-            changeKey(){
-            this.currentTarget = this.notes.id = this.currentTarget.id
-            this.isActive = !this.isActive
-            },
-        },
-        destroyed() {
-            document.removeEventListener('keydown', this.keySound);
-        },
-    }
+            changeKey() {
+             let findKey = this.notes.find(el=>el.keyCode === this.textKey)
+                if (findKey){
+                    this.error = `Same character exist put other ${this.textKey.toUpperCase()}`
+                }
+                else {
+                    this.currentTarget.keyCode = this.textKey
+                    localStorage.setItem(`${this.currentTarget.name}`, this.textKey)
+                    this.isActive = !this.isActive
+                }
 
+                },
+            cancelButton() {
+                this.isActive = false
+                this.textKey = ''
+            }
+        },
+        mounted() {
+         this.notes.forEach(el=>{
+             const name = localStorage.getItem(el.name);
+             if (name){
+                 this.notes[el.id-1].keyCode = name
+             }
+         })
+        }
+    }
 
 
 </script>
 
 <style scoped>
-.container{
-    margin-left: 25%;
-    margin-top: 100px;
-    height: 250px;
-    width: 1200px;
-    display: flex;
-    flex-direction: row;;
-}
-    .piano{
+    @import "~materialize-css/dist/css/materialize.min.css";
+
+    .text-inp {
+        border: 1px solid black;
+        width: 300px;
+    }
+
+    .buttons-input {
+        display: flex;
+        justify-content: space-around;
+    }
+
+    .container {
+        margin-left: 25%;
+        margin-top: 100px;
+        height: 250px;
+        width: 1200px;
+        display: flex;
+        flex-direction: row;;
+    }
+
+    .piano {
         display: flex;
         width: auto;
         height: 250px;
         flex-direction: row;
     }
-.button-piano{
-    width: 80px;
-    background-color: burlywood;
-    border-radius: 33%;
-    border: 1px solid black;
-    text-align: center;
 
-}
-.wave2 {
-    -webkit-animation: wave-animation1 6.3s infinite linear; /* Safari 4+ */
-    -moz-animation:    wave-animation1 6.3s infinite linear; /* Fx 5+ */
-    -o-animation:      wave-animation1 6.3s infinite linear; /* Opera 12+ */
-    animation:         wave-animation1 6.3s infinite linear; /* IE 10+ */
-}
+    .button-piano {
+        width: 80px;
+        background-color: burlywood;
+        border-radius: 20%;
+        border: 1px solid black;
+        text-align: center;
 
-@-webkit-keyframes wave-animation1 {
-    0%   { background-position: 0 0; }
-    100% { background-position: 1601px 0; }
-}
-@-moz-keyframes wave-animation1 {
-    0%   { background-position: 0 0; }
-    100% { background-position: 1601px 0; }
-}
-@-o-keyframes wave-animation1 {
-    0%   { background-position: 0 0; }
-    100% { background-position: 1601px 0; }
-}
-@keyframes wave-animation1 {
-    0%   { background-position: 0 0; }
-    100% {background-position: 1601px 0;}
-}
+    }
 
-.wave2 {
-    background: url('http://www.templates-preview.com/bootstrap-templates/300111854/images/wave2.png') 0 0 repeat-x;
-}
+    .wave2 {
+        -webkit-animation: wave-animation1 6.3s infinite linear; /* Safari 4+ */
+        -moz-animation: wave-animation1 6.3s infinite linear; /* Fx 5+ */
+        -o-animation: wave-animation1 6.3s infinite linear; /* Opera 12+ */
+        animation: wave-animation1 6.3s infinite linear; /* IE 10+ */
+    }
+
+    @-webkit-keyframes wave-animation1 {
+        0% {
+            background-position: 0 0;
+        }
+        100% {
+            background-position: 1601px 0;
+        }
+    }
+
+    @-moz-keyframes wave-animation1 {
+        0% {
+            background-position: 0 0;
+        }
+        100% {
+            background-position: 1601px 0;
+        }
+    }
+
+    @-o-keyframes wave-animation1 {
+        0% {
+            background-position: 0 0;
+        }
+        100% {
+            background-position: 1601px 0;
+        }
+    }
+
+    @keyframes wave-animation1 {
+        0% {
+            background-position: 0 0;
+        }
+        100% {
+            background-position: 1601px 0;
+        }
+    }
+
+    .wave2 {
+        background: url('http://www.templates-preview.com/bootstrap-templates/300111854/images/wave2.png') 0 0 repeat-x;
+    }
 
 
 </style>
